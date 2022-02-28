b515bdc16548bacf0b4cd46675e5fb52bae222ea
Surajram1707 <surajram1707@gmail.com>
2022-02-28 12:15:38 +0530

    done changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {QuicknotesComponent} from './quicknotes/quicknotes.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuicknotesComponent
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
